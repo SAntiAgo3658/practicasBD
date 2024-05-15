01a45bdbb6b6fbe41b7a45fc422ba42577574c9d
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2024-05-15 13:18:56 +0200

    Adrián se tiene que ir

--- src/nuestroCRUD/MiCRUD.java
+++ src/nuestroCRUD/MiCRUD.java
@@ -60,6 +60,33 @@ public class MiCRUD {
 
    // create
    public boolean createTable(String name, MyColumn[] columns, MyConstraint[] constraints){
+      String myQuery = "CREATE TABLE ".concat(name) + " (";
+      for (int i = 0; i < columns.length; i++) {
+         myQuery = myQuery + columns[i].getColName() + " " + columns[i].getColType() + " ";
+         if (columns[i].isNulleable()) {
+            myQuery = myQuery + ",";
+         } else {
+            myQuery = myQuery + "NOT NULL" + ", ";
+         }
+         // There´s always at least one constraint.
+      }
+      for (int i = 0; i < constraints.length - 1; i++) {
+         myQuery = myQuery + " CONSTRAINT " + constraints[i].getParams()[0];
+         if (constraints[i].getParams().length == 2){ // Primary
+            myQuery = myQuery + " PRIMARY KEY (";
+            myQuery = myQuery + constraints[j].getParams()[1] + ")," 
+
+         } else { // Foreign
+            myQuery = myQuery + " FOREIGN KEY (";
+            myQuery = myQuery + constraints[i].getParams()[1] + ") ";
+            myQuery = myQuery + " REFERENCES ";
+            myQuery = myQuery + constraints[i].getParams()[2] + "(";
+            myQuery = myQuery + constraints[i].getParams()[3] + "),";
+
+
+         }
+         
+      }
 
    }
    // read
