4417a80ff039f2635d62d68a7204929d66f85e87
SAntiAgo3658 <santiagoelias.s373384@cesurformacion.com>
2024-05-20 13:27:58 +0200

    error ilegal

--- src/App.java
+++ src/App.java
@@ -8,17 +8,35 @@ import nuestroCRUD.MyConstraint;
 public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         
-        MiCRUD prueba = new MiCRUD("bank");
+        MiCRUD miCRUD = new MiCRUD("bank");
 
-        MyColumn[] columnas = new MyColumn[1];
+        MyColumn[] columnas = new MyColumn[2];
         columnas[0] = new MyColumn();
         columnas[0].setColName("nombre");
         columnas[0].setColType("varchar(32)");
-        MyConstraint[] restricciones = new MyConstraint[1];
+        columnas[1] = new MyColumn();
+        columnas[1].setColName("apellido");
+        columnas[1].setColType("varchar(32)");
+        columnas[1].setNulleable(true);
+        MyConstraint[] restricciones = new MyConstraint[3];
         restricciones[0] = new MyConstraint(true);
         restricciones[0].setParams(new String[] {"pk_nombre", "nombre"});
+
+        miCRUD.initDriver();
+        miCRUD.initConnection();
+        miCRUD.createStatement();
+        String miQuery = miCRUD.createTable("enfermos", columnas, restricciones);
         
-        System.out.println(prueba.createTable("mi tabla", columnas , restricciones));
+        if (miCRUD.useStatemente(miQuery)) {
+            System.out.println("Creada tabla");
+            
+        } else {
+            System.out.println("ERROR!!");
+        }
+
+        
+        
+        miCRUD.closeConnection();
 
     }
 }
